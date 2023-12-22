940382365d6252a0204f21a722f7660678bafc4a
iraahdani <iraahdanisiregar@gmail.com>
2023-12-22 22:05:18 +0700

    revisi edit_kategori

--- kategori_edit/update_kategori.php
+++ kategori_edit/update_kategori.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Obat Edit</title>
+        <title>Kategori Edit</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="asset/css/style.css" />
